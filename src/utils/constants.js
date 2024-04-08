fce827708a7ccca3215ae65cc7c711d66d2d3ce1
wckedwisdom0911 <shivamrawat091125@gmail.com>
2024-04-08 17:35:52 +0530

    Initial commit

---
+++ src/utils/constants.js
@@ -0,0 +1,12 @@
+
+
+
+const API_KEY="AIzaSyB25o3lOO8TO_0YYXbkr1TahGV7e73kzqM"
+
+
+export const YOUTUBE_API="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&key=AIzaSyB25o3lOO8TO_0YYXbkr1TahGV7e73kzqM"
+
+export const YOUTUBE_SEARCH_API =
+  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+
+  export const LIVE_CHAT_COUNT = 25;
